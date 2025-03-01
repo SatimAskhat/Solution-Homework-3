2462096f41f9bd270405b3456f87e1af117673f9
SatimAskhat <s23067494@narxoz.kz>
2025-03-01 11:50:20 +0500

    Update IDungeonBuilder.java

--- src/builder/IDungeonBuilder.java
+++ src/builder/IDungeonBuilder.java
@@ -1,1 +1,6 @@
-
+interface IDungeonBuilder {
+    IDungeonBuilder setDungeonName(String name);
+    IDungeonBuilder addRoom(Room room);
+    IDungeonBuilder addNPC(NPC npc);
+    Dungeon build();
+}

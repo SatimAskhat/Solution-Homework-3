b8714bfed6d6ee16eaa5968e88df3ffa7b6ae1b3
SatimAskhat <s23067494@narxoz.kz>
2025-03-01 11:52:39 +0500

    Update Room.java

--- src/prototype/Room.java
+++ src/prototype/Room.java
@@ -1,1 +1,19 @@
-
+class Room implements CloneableGameEntity {
+    private String name;
+    private String description;
+    
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+    
+    @Override
+    public Room cloneEntity() {
+        return new Room(this.name, this.description);
+    }
+    
+    @Override
+    public String toString() {
+        return "Room: " + name + " (" + description + ")";
+    }
+}

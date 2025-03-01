b30580da025e6462282bf9148300db313b3fc0c5
SatimAskhat <s23067494@narxoz.kz>
2025-03-01 11:51:41 +0500

    Update MUDPrototypeDemo.java

--- src/demo/MUDPrototypeDemo.java
+++ src/demo/MUDPrototypeDemo.java
@@ -1,1 +1,14 @@
-
+class MUDPrototypeDemo {
+    public static void main(String[] args) {
+        Room originalRoom = new Room("Library", "Filled with old books");
+        Room clonedRoom = originalRoom.cloneEntity();
+        
+        NPC originalNPC = new NPC("Mage", "Master of fire magic");
+        NPC clonedNPC = originalNPC.cloneEntity();
+        
+        System.out.println("Original Room: " + originalRoom);
+        System.out.println("Cloned Room: " + clonedRoom);
+        System.out.println("Original NPC: " + originalNPC);
+        System.out.println("Cloned NPC: " + clonedNPC);
+    }
+}

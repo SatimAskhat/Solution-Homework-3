39b318816e3c4cda4a0cae440555812475e2b163
SatimAskhat <s23067494@narxoz.kz>
2025-03-01 11:52:17 +0500

    Update NPC.java

--- src/prototype/NPC.java
+++ src/prototype/NPC.java
@@ -1,1 +1,19 @@
-
+class NPC implements CloneableGameEntity {
+    private String name;
+    private String role;
+    
+    public NPC(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+    
+    @Override
+    public NPC cloneEntity() {
+        return new NPC(this.name, this.role);
+    }
+    
+    @Override
+    public String toString() {
+        return "NPC: " + name + " (" + role + ")";
+    }
+}

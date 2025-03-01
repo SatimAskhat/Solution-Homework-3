7d644726169d1cc61150bd40bcc190771990293d
SatimAskhat <s23067494@narxoz.kz>
2025-03-01 11:51:04 +0500

    Update MUDBuilderDemo.java

--- src/demo/MUDBuilderDemo.java
+++ src/demo/MUDBuilderDemo.java
@@ -1,1 +1,12 @@
-
+class MUDBuilderDemo {
+    public static void main(String[] args) {
+        Dungeon dungeon = new SimpleDungeonBuilder()
+                .setDungeonName("Ancient Ruins")
+                .addRoom(new Room("Entrance Hall", "Dark and cold"))
+                .addRoom(new Room("Throne Room", "Golden decorations everywhere"))
+                .addNPC(new NPC("Guardian", "Protects the ruins"))
+                .build();
+        
+        System.out.println(dungeon);
+    }
+}

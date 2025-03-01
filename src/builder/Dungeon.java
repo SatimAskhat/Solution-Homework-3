0e249b7b6d4f209b1874f0aaba83e26bac52d827
SatimAskhat <s23067494@narxoz.kz>
2025-03-01 11:49:49 +0500

    Update Dungeon.java

--- src/builder/Dungeon.java
+++ src/builder/Dungeon.java
@@ -1,1 +1,16 @@
+class Dungeon {
+    private String name;
+    private List<Room> rooms = new ArrayList<>();
+    private List<NPC> npcs = new ArrayList<>();
 
+    public Dungeon(String name, List<Room> rooms, List<NPC> npcs) {
+        this.name = name;
+        this.rooms = rooms;
+        this.npcs = npcs;
+    }
+    
+    @Override
+    public String toString() {
+        return "Dungeon: " + name + "\nRooms: " + rooms + "\nNPCs: " + npcs;
+    }
+}

45ff8c2fb73fb9c3c1777c28aa6b94adf39e33a3
SatimAskhat <s23067494@narxoz.kz>
2025-03-01 11:51:25 +0500

    Update MUDCombinedDemo.java

--- src/demo/MUDCombinedDemo.java
+++ src/demo/MUDCombinedDemo.java
@@ -1,1 +1,18 @@
-
+class MUDCombinedDemo {
+    public static void main(String[] args) {
+        Room room1 = new Room("Treasure Room", "Filled with gold");
+        NPC boss = new NPC("Dragon", "Final Boss");
+        
+        Dungeon dungeon = new SimpleDungeonBuilder()
+                .setDungeonName("Dark Cave")
+                .addRoom(room1)
+                .addNPC(boss)
+                .build();
+        
+        System.out.println(dungeon);
+        
+        // Клонирование комнаты
+        Room clonedRoom = room1.cloneEntity();
+        System.out.println("Cloned Room: " + clonedRoom);
+    }
+}
